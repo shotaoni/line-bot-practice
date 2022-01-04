4407a0d3a69f2a9166f0a6e66c9d98f5f3e233e3
shotaonizuka <syota.oni@gmail.com>
2022-01-04 21:37:25 +0900

    add おうむ返し機能

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module line-bot-practice
 
 go 1.17
+
+require github.com/line/line-bot-sdk-go v7.8.0+incompatible
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- hello/main.go
+++ hello/main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
 	// ハンドラの登録
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/callback", lineHandler)
 
 	fmt.Println("http://localhost:8080")
 	// HTTPサーバーを起動
@@ -18,4 +23,49 @@ func main() {
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	msg := "Hello World!"
 	fmt.Fprintf(w, msg)
+}
+
+func lineHandler(w http.ResponseWriter, r *http.Request) {
+	err := godotenv.Load(fmt.Sprintf("envfiles/%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		// .env読めなかった場合の処理s
+		log.Fatal(err)
+	}
+	secret := os.Getenv("LINE_BOT_CHANNEL_SECRET")
+	token := os.Getenv("LINE_BOT_CHANNEL_TOKEN")
+
+	// BOTを初期化
+	bot, err := linebot.New(
+		secret,
+		token,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// リクエストからBOTのイベントを取得
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		// イベントがメッセージの受信だった場合
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			// メッセージがテキスト形式の場合
+			case *linebot.TextMessage:
+				replyMessage := message.Text
+				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				if err != nil {
+					log.Print(err)
+				}
+			}
+			// 他にもスタンプや画像、位置情報などいろいろ受信できる
+		}
+	}
 }
